768277777b0f39dbb3601b581165cae64fea379b
user <https://github.com/NikitaBurtelov/RubikCube.git>
2020-05-12 19:31:22 +0300

    fix

--- src/main/java/connect/Connect.java
+++ src/main/java/connect/Connect.java
@@ -3,6 +3,12 @@ package connect;
 import com.petersamokhin.bots.sdk.clients.Group;
 import com.petersamokhin.bots.sdk.objects.Message;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+
 public class Connect {
     private static final int idGroup = 195134131;
     private static final String token = "d9d8dd3f78413d15d0e443763f56d1ca1983196de290317b109e06e12565a1779e6224f3b72f612da5c1d";
@@ -15,7 +21,7 @@ public class Connect {
                     .from(group)
                     .to(message.authorId())
                     .text("Пиздуй ботать, а не трепаться !!!!!")
-                    .photo("connect\\picture_2.jpg")
+                    .photo("target\\classes\\picture_2.jpg")
                     .send()
         );
 
