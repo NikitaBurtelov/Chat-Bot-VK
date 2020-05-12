9ec5cf41370cbaac3b26605012b62ba285702cf1
bnn18u166 <nikita.burtelov@mail.ru>
2020-05-12 22:08:39 +0300

    add photo and sticer message

--- ChatBotVk/src/main/java/connect/Connect.java
+++ ChatBotVk/src/main/java/connect/Connect.java
@@ -5,16 +5,35 @@ import com.petersamokhin.bots.sdk.objects.Message;
 
 public class Connect {
     private static final int idGroup = 195134131;
-    private static final String token = "idi_.....(очень далеко)";
+    private static final String token = "<token>";
 
     public static void main(String[] args) {
         Group group = new Group(idGroup, token);
+
         group.onSimpleTextMessage(message ->
                 new Message()
-                    .from(group)
-                    .to(message.authorId())
-                    .text("Пообщаемся???")
-                    .send()
+                        .from(group)
+                        .to(message.authorId())
+                        .text("Иди ботай !!!!!")
+                        .photo("target/classes/image/picture_2.jpg")
+                        .send()
         );
+
+        group.onPhotoMessage(message ->
+                new Message()
+                .from(group)
+                .to(message.authorId())
+                .text("Ну и зачем мне твои нужны твои картинки ???")
+                .send()
+        );
+
+        group.onStickerMessage(message ->
+                new Message()
+                        .from(group)
+                        .to(message.authorId())
+                        .text("Иииии ????")
+                        .send()
+        );
+
     }
 }
