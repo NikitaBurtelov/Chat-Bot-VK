b4a00d6247689c0910134e6d579615423a0430a9
user <https://github.com/NikitaBurtelov/RubikCube.git>
2020-05-12 21:53:41 +0300

    fix

--- src/main/java/connect/Connect.java
+++ src/main/java/connect/Connect.java
@@ -3,6 +3,12 @@ package connect;
 import com.petersamokhin.bots.sdk.clients.Group;
 import com.petersamokhin.bots.sdk.objects.Message;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+
 public class Connect {
     private static final int idGroup = 195134131;
     private static final String token = "d9d8dd3f78413d15d0e443763f56d1ca1983196de290317b109e06e12565a1779e6224f3b72f612da5c1d";
@@ -14,9 +20,27 @@ public class Connect {
                 new Message()
                     .from(group)
                     .to(message.authorId())
-                    .text("Иди ботай !!!!")
+                    .text("Пппц")
                     .photo("/res/image/picture_2.jpg")
+                        .photo("res/image/picture_2.jpg")
+                        .photo("\\res\\image\\picture_2.jpg")
+                        .photo("res\\image\\picture_2.jpg")
                     .send()
+        );
+
+        group.onSimpleTextMessage(message ->
+                new Message()
+                        .from(group)
+                        .to(message.authorId())
+                        .text("1")
+                        .photo("/res/image/picture_2.jpg")
+                        .text("2")
+                        .photo("res/image/picture_2.jpg")
+                        .text("3")
+                        .photo("\\res\\image\\picture_2.jpg")
+                        .text("4")
+                        .photo("res\\image\\picture_2.jpg")
+                        .send()
         );
 
         group.onPhotoMessage(message ->
