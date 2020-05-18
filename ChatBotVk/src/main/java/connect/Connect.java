86b848fac2e771eb2cac0aefa55bb35c9d87738b
bnn18u166 <nikita.burtelov@mail.ru>
2020-05-18 21:27:45 +0300

    add check message

--- ChatBotVk/src/main/java/connect/Connect.java
+++ ChatBotVk/src/main/java/connect/Connect.java
@@ -1,23 +1,44 @@
 package connect;
 
 import com.petersamokhin.bots.sdk.clients.Group;
+import com.petersamokhin.bots.sdk.clients.User;
 import com.petersamokhin.bots.sdk.objects.Message;
+import handler.CheckMessage;
 
 public class Connect {
     private static final int idGroup = 195134131;
-    private static final String token = "<token>";
+    private static final String token = "token";
 
     public static void main(String[] args) {
         Group group = new Group(idGroup, token);
+        User user = new User(token);
 
-        group.onSimpleTextMessage(message ->
+        user.onMessage(message -> {
+            String textMessage = message.getText();
+
+            if (new CheckMessage(textMessage).WelcomeCheck()) {
                 new Message()
+                        .from(user)
+                        .to(message.authorId())
+                        .text("Hello, user!")
+                        .send();
+
+            } else if (textMessage.equalsIgnoreCase("Пока")) {
+                new Message()
+                        .from(user)
+                        .to(message.authorId())
+                        .text("До скорого")
+                        .send();
+            } else {
+                new Message()
+                        .attachments()
                         .from(group)
                         .to(message.authorId())
                         .text("Иди ботай !!!!!")
                         .photo("target/classes/image/picture_2.jpg")
-                        .send()
-        );
+                        .send();
+            }
+        });
 
         group.onPhotoMessage(message ->
                 new Message()
@@ -34,6 +55,5 @@ public class Connect {
                         .text("Иииии ????")
                         .send()
         );
-
     }
 }
