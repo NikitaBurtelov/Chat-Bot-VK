438b0773fb18b50f33fbc497b2adb1c49ee12024
bnn18u166 <nikita.burtelov@mail.ru>
2020-05-20 02:51:37 +0300

    game search implementation

--- ChatBotVk/src/main/java/connect/Connect.java
+++ ChatBotVk/src/main/java/connect/Connect.java
@@ -4,12 +4,15 @@ import com.petersamokhin.bots.sdk.clients.Group;
 import com.petersamokhin.bots.sdk.clients.User;
 import com.petersamokhin.bots.sdk.objects.Message;
 import handler.CheckMessage;
+import parser.PageInfo;
+
+import java.io.IOException;
 
 public class Connect {
     private static final int idGroup = 195134131;
-    private static final String token = "token";
+    private static final String token = "d9d8dd3f78413d15d0e443763f56d1ca1983196de290317b109e06e12565a1779e6224f3b72f612da5c1d";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Group group = new Group(idGroup, token);
         User user = new User(token);
 
@@ -29,6 +32,13 @@ public class Connect {
                         .to(message.authorId())
                         .text("До скорого")
                         .send();
+            }
+            else if (textMessage.contains("/game")) {
+                    new Message()
+                            .from(user)
+                            .to(message.authorId())
+                            .text(new PageInfo(textMessage).runSearch())
+                            .send();
             } else {
                 new Message()
                         .attachments()
