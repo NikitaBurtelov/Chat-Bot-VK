7e9dfb2278b0bd04b2bc62621ad51a9d3d530fe4
bnn18u166 <nikita.burtelov@mail.ru>
2020-06-20 18:21:47 +0300

    add jdbc driver pom

--- ChatBotVk/src/main/java/game/hangman/GameDataBase.java
+++ ChatBotVk/src/main/java/game/hangman/GameDataBase.java
@@ -6,9 +6,9 @@ public class GameDataBase {
     private static Connection con;
     private static Statement stmt;
     private static ResultSet rs;
-    String userName;
-    String password;
-    String connectionUrl;
+    private static String userName = "";
+    private static String password = "";
+    private static String connectionUrl = "jdbc:mysql://eu-cdbr-west-03.cleardb.net:3306/heroku_5f088a083235cc3";
 
     public GameDataBase(String userName, String password, String connectionUrl) {
         this.userName = userName;
@@ -16,23 +16,28 @@ public class GameDataBase {
         this.connectionUrl = connectionUrl;
     }
 
-    private void setConnectionUrl() throws SQLException, ClassNotFoundException {
-        Class.forName("com.mysql.jdbc.Driver");
-        String query = "select id, words from wordshangman";
+    private static void setConnectionUrl() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String query = "select id, words from wordshangman";
+            con = DriverManager.getConnection(connectionUrl, userName, password);
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(query);
 
-        while (rs.next()) {
-            int id = rs.getInt(1);
-            String word = rs.getString(2);
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                String word = rs.getString(2);
 
-            System.out.println(id + " " + word);
+                System.out.println(id + " " + word);
         }
-
-        try (Connection connection = DriverManager.getConnection(connectionUrl, userName, password);
-             Statement statement = connection.createStatement()) {
-
-            statement.close();
+        stmt.close();
+        } catch (SQLException | ClassNotFoundException exception) {
+            exception.printStackTrace();
         }
+    }
 
+    public static void main(String[] args) {
+        setConnectionUrl();
     }
 
 }
