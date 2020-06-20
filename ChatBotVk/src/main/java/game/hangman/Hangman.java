ca3d0eb28ac87f73aa8b077e71ab6ef1af563f24
bnn18u166 <nikita.burtelov@mail.ru>
2020-06-20 03:33:08 +0300

    add getLetter

--- ChatBotVk/src/main/java/game/hangman/Hangman.java
+++ ChatBotVk/src/main/java/game/hangman/Hangman.java
@@ -1,12 +1,20 @@
 package game.hangman;
 
+import com.petersamokhin.bots.sdk.clients.User;
+import com.petersamokhin.bots.sdk.objects.Message;
+import handler.CheckMessage;
+
 import java.util.Random;
 
 public class Hangman {
     public static String word = "джава";
     public static byte attempt = 6;
     public static int lenWorld;
+    private static User user;
 
+    Hangman(User user) {
+        this.user = user;
+    }
     //получаем рандомное слово из бд
     public static void randomWord() {
         Random random = new Random();
@@ -17,17 +25,49 @@ public class Hangman {
 
     public static boolean checkLetter(String letter) {
         int lenNow;
-        while (attempt > 0) {
-            word = word.replaceAll(letter, "");
-            lenNow = word.length();
-            if (lenWorld == lenNow) {
-                attempt--;
-            }
-            else if (lenNow == 0)
-                return true;
-            else
-                lenWorld = lenNow;
+
+        word = word.replaceAll(letter, "");
+        lenNow = word.length();
+
+        if (lenWorld == lenNow) {
+            attempt--;
+            return false;
         }
+        else {
+            lenWorld = lenNow != 0? lenNow: -1;
+
+            return true;
+        }
+    }
+
+    public static boolean runGame() {
+        randomWord(); //...--> word, lenWord
+        user.onMessage(message -> {
+            String textMessage = message.getText();
+            Message gameMessage = new Message();
+
+            gameMessage
+                    .from(user)
+                    .to(message.authorId())
+                    .text(checkLetter(textMessage)?"Угадал":"Не угадал")
+                    .send();
+
+            if (attempt == 0) {
+                gameMessage
+                        .from(user)
+                        .to(message.authorId())
+                        .text("Проиграл")
+                        .send();
+            }
+            else if (lenWorld == -1) {
+                gameMessage
+                        .from(user)
+                        .to(message.authorId())
+                        .text("Победа, вот слово - " + word)
+                        .send();
+            }
+        });
+
         return false;
     }
 }
