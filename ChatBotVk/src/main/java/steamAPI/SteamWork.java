dbe4144550558446d2b36b9eab58fddbdd442efc
bnn18u166 <nikita.burtelov@mail.ru>
2020-07-19 20:06:00 +0300

    add steamApi / work with the Steam

---
+++ ChatBotVk/src/main/java/steamAPI/SteamWork.java
@@ -0,0 +1,13 @@
+package steamAPI;
+
+public class SteamWork {
+    private static String url = "https://store.steampowered.com/search/?term=";
+
+    private static String searchId (String title) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
